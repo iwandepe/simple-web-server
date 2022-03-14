01ca3d5c5f01bfd0342fdc7d2b570aa32db1c1b6
Allam Taju Sarof <55141556+Allam0053@users.noreply.github.com>
2022-03-14 15:02:30 +0700

    nomor 5

--- src/com/progjar/server/Main.kt
+++ src/com/progjar/server/Main.kt
@@ -2,6 +2,7 @@ package com.progjar.server
 
 import java.io.*
 import java.net.ServerSocket
+import java.net.Socket
 
 
 /**
@@ -14,12 +15,16 @@ import java.net.ServerSocket
  * 5. Keeps the connection open if the client requests it. (Hint: check the Connection HTTP header. Your webserver will not be able to accept another client once the connection is still open, but that is okay)
  */
 
-val ROOTDIR = "/home/iwandepe/progjar/www/"
+//val ROOTDIR = "/home/iwandepe/progjar/www/"
+val ROOTDIR = "C:\\developing\\project\\project-java\\simple-web-server\\res\\"
 val PORT = 8888
+val REQUEST_TIMEOUT = 3L
 
-fun main() {
+var clientAlive : Socket? = null
+
+fun main(args: Array<String>) {
     try {
-        val server = ServerSocket(PORT)
+        val server = ServerSocket( PORT )
 
         while (true) {
             val client = server.accept()
@@ -28,38 +33,103 @@ fun main() {
             val out = PrintWriter(client.getOutputStream())
 
             var message = br.readLine()
+            println( "----connection accepted----" )
             println("Request: $message")
 
             var urn = message.split(" ")[1]
             urn = urn.substring( 1 )
 
+            var isKeepAlive = false
+            var isChrome = false
             while(!message.isEmpty()) {
                 message = br.readLine()
+
+                if (message.contains("keep-alive", true)) {
+                    isKeepAlive = true
+                }
+                if (message.contains("chrome", true) || message.contains("Mozilla", true)) {
+                    isChrome = true
+                }
             }
 
-            var f = File(ROOTDIR + urn)
-            if (f.exists() && !f.isDirectory()) {
-                if (f.extension == "html"){
-                    responseHtmlFile(out, f)
-                } else {
-                    responseBinaryFile(out, f)
-                }
-            } else if (f.exists() && f.isDirectory()) {
-                var fIndex = File(ROOTDIR + urn + "index.html")
-                if (fIndex.exists()) {
-                    responseHtmlFile(out, fIndex)
-                } else {
-                    responseDirectoryListing(out, f)
-                }
-            } else {
-                responseByStatusCode(out, 404)
+            response( out, urn )
+
+            /*dont keep chrome alive*/
+            if (isKeepAlive && !isChrome) {
+                println( client.keepAlive )
+                println( "----connection still alive----" )
+                keepAlive( client )
             }
 
+            println( "----connection closed----\n" )
             client.close()
         }
         server.close()
     } catch (e: Exception) {
         println(e.message)
+    }
+}
+
+fun keepAlive( client: Socket ) {
+    println( '1' )
+    var keepAlive = false
+
+    var br = BufferedReader(InputStreamReader(client.getInputStream()))
+    var message: String? = ""
+    try{
+        message = br.readLine()
+        println( "**empty input**" )
+    } catch (e: IOException) {
+        println( "**empty input**" )
+        message = ""
+        return
+    }
+
+    val out = PrintWriter(client.getOutputStream())
+    println("Request: $message")
+    if (message.isNullOrEmpty()) {
+        message = ""
+        return
+    }
+
+    var urn = message.split(" ")[1]
+    urn = urn.substring( 1 )
+
+    println( '2' )
+    while(!message!!.isEmpty()) {
+        message = br.readLine()
+
+        if (message.contains("keep-alive")) {
+            keepAlive = true
+        }
+    }
+    println( '3' )
+
+    response( out, urn )
+
+    if (keepAlive == true) {
+        println( "----connection still alive----\n" )
+        keepAlive( client )
+    } else return
+}
+
+fun response( out: PrintWriter, urn: String ) {
+    var f = File(ROOTDIR + urn)
+    if (f.exists() && !f.isDirectory()) {
+        if (f.extension == "html"){
+            responseHtmlFile(out, f)
+        } else {
+            responseBinaryFile(out, f)
+        }
+    } else if (f.exists() && f.isDirectory()) {
+        var fIndex = File(ROOTDIR + urn + "index.html")
+        if (fIndex.exists()) {
+            responseHtmlFile(out, fIndex)
+        } else {
+            responseDirectoryListing(out, f)
+        }
+    } else {
+        responseByStatusCode(out, 404)
     }
 }
 
@@ -76,7 +146,7 @@ fun responseHtmlFile(out: PrintWriter, f: File) {
     out.flush()
 }
 
-// Not working right
+// Not working correctly
 fun responseBinaryFile(out: PrintWriter, f: File) {
     val fis = FileInputStream(f)
 
@@ -99,7 +169,7 @@ fun responseDirectoryListing(out: PrintWriter, f: File) {
     var response = "<ul>"
     for (file in fileList) {
         response += "<li>"
-        response += "<a href=\"" + f.path.substring(ROOTDIR.length) + "/" + file.name + "\">"
+        response += "<a href=\"" + f.path.substring(ROOTDIR.length - 1) + "/" + file.name + "\">"
 
         if (file.isDirectory()) {
             response += file.name + "/\n"
@@ -126,7 +196,7 @@ fun responseByStatusCode(out: PrintWriter, code: Int) {
         status = "404 Not Found"
     }
 
-    out.println("HTTP/1.0" + status)
+    out.println("HTTP/1.0 " + status)
     out.println("Content-Type: html")
     out.println()
     out.println(status)
